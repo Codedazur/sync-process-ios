b9b8b0413d7b4d58463a08494898d22567a000c1
Tamara Bernad <tamarinda@gmail.com>
2016-04-14 15:58:46 +0200

    fixed json and compile error

--- Pod/Classes/Utils/CDACoreDataStackProtocol.h
+++ Pod/Classes/Utils/CDACoreDataStackProtocol.h
@@ -13,7 +13,7 @@
 @property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
 @property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
 @property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
-
++ (NSManagedObjectContext *)independentManagedObjectContext;
 - (NSManagedObject *)fetchEntity:(NSString *)entity WithPredicate:(NSPredicate *)predicate InContext:(NSManagedObjectContext *)context;
 - (NSArray *)fetchEntities:(NSString *)entity WithSortKey:(NSString *)sortKey Ascending:(BOOL)ascending WithPredicate:(NSPredicate *)predicate InContext:(NSManagedObjectContext *)context;
 + (instancetype)initSharedInstanceWithModelName:(NSString *)modelName;
