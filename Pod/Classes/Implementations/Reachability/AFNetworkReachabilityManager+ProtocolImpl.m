f0252e27cd5241b2b3d2af2c2936e5a169eb3614
Tamara Bernad <tamarinda@gmail.com>
2016-05-04 15:48:09 +0200

    added reachability

---
+++ Pod/Classes/Implementations/Reachability/AFNetworkReachabilityManager+ProtocolImpl.m
@@ -0,0 +1,13 @@
+//
+//  AFNetworkReachabilityManager+ProtocolImpl.m
+//  Pods
+//
+//  Created by Tamara Bernad on 04/05/16.
+//
+//
+
+#import "AFNetworkReachabilityManager+ProtocolImpl.h"
+
+@implementation AFNetworkReachabilityManager (ProtocolImpl)
+
+@end
